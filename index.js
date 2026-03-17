4252f630459184c6af20f2a10605107f35676ee6
Atharva M <atharva.mainds83465@gmail.com>
2026-03-18 02:52:14 +0530

    test redeploy after restart

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from my EC2 server!</h1>')
+  res.send('<h1>hey handsome</h1>')
 })
 
 app.listen(3000, () => {
